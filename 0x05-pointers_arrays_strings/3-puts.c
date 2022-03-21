44f8997d9c9174d5c84336fdc45a1346cccbda82
Ege Grace <99275257+OriginalGracias@users.noreply.github.com>
2022-03-21 21:47:31 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by new line to stdout
+ * @str: string
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
