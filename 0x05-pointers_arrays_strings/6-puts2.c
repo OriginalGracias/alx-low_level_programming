6e9081bd1aa7bcaae384df6bcdb7d04486ab7e4b
Ege Grace <99275257+OriginalGracias@users.noreply.github.com>
2022-03-22 17:37:58 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - print other charcter of string starting from first charcter
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
 
