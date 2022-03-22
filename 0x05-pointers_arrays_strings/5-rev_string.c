68b6db1e0645ccd764aced656e9c0d2874eae22f
Ege Grace <99275257+OriginalGracias@users.noreply.github.com>
2022-03-22 05:18:29 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+/**
+ * rev_string - prints a string in reverse
+ * @s: string
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		len++;
+		++s;
+	}
+
+	s--;
+
+	for (i = len; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+
+
+}
