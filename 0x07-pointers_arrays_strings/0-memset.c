fd0628c346a4c756af64fc2655378cff51892bfb
Ege Grace <99275257+OriginalGracias@users.noreply.github.com>
2022-03-29 00:38:35 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,10 +8,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; i < n; i++)
-*(s + i) =  b;
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
 
-return (s);
+	return (s);
 }
