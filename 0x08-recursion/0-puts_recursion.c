fe001861db15b5054deb7ee67769614afe1df552
Ege Grace <99275257+OriginalGracias@users.noreply.github.com>
2022-03-30 23:47:44 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
-
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string
- * Return: no return.
- */
+ * _puts_recursion - Prints a string with recursion.
+ * Desc: _memset
+ * @s: char type
+ * Return: char type
+ **/
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+else
+_putchar(*s);
+s++;
+_puts_recursion(s);
 }
